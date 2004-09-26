5cd1982dd2395144dfed1b5ff24a73448106dc77
fifteenknots505 <fifteenknots505@gmail.com>
2004-09-26 17:07:06 +0000

    aufgeräumt und verschoben.

--- src/de/marw/nacre/editorkits/C_Categoriser.java
+++ src/de/marw/nacre/editorkits/C_Categoriser.java
@@ -1,35 +1,58 @@
-// $Header$
+// $Header:
+// /home/weber/cvsRepos/highlighting/swing/text/highlight/C_Categoriser.java,v
+// 1.1
+// 2004/09/26 13:36:30 weber Exp $
 
-package swing.text.highlight;
+package swing.text.highlight.categoriser;
 
 import java.text.CharacterIterator;
+import java.util.Arrays;
+import java.util.Comparator;
 
 import javax.swing.text.Element;
 import javax.swing.text.Segment;
 
-import swing.text.highlight.categoriser.AbstractCategoriser;
-import swing.text.highlight.categoriser.CategoryConstants;
-import swing.text.highlight.categoriser.Token;
+import swing.text.highlight.HighlightedDocument;
 
 
-public class C_Tokeniser extends AbstractCategoriser
+public class C_Categoriser extends AbstractCategoriser
 {
 
-  private static final String[] kwPredefVal = { "true", "false" };
-
-  private static final String[] kwType      = { "char", "double", "enum",
-      "float", "int", "long", "short", "signed", "struct", "typedef", "union",
-      "unsigned", "void", "auto", "const", "extern", "register", "static",
-      "volatile", "far", "huge", "inline", "near", "pascal" };
-
-  private static final String[] kwStmt      = { "asm", "break", "case",
-      "continue", "default", "do", "else", "for", "goto", "if", "return",
-      "switch", "while"                    };
-
-  private static final String[] kwOperator  = { "sizeof" };
-
-  C_Tokeniser()
+  public static final class RevStringByLengthComparator implements Comparator
   {
+    public int compare( Object o1, Object o2)
+    {
+      return ((String) o2).length() - ((String) o1).length();
+    }
+  }
+
+  private static final String[]              kwPredefVal        = { "true",
+      "false"                                                  };
+
+  private static final String[]              kwType             = { "char",
+      "double", "enum", "float", "int", "long", "short", "signed", "struct",
+      "typedef", "union", "unsigned", "void", "auto", "const", "extern",
+      "register", "static", "volatile", "far", "huge", "inline", "near",
+      "pascal"                                                 };
+
+  private static final String[]              kwStmt             = { "asm",
+      "break", "case", "continue", "default", "do", "else", "for", "goto",
+      "if", "return", "switch", "while"                        };
+
+  private static final String[]              kwOperator         = { "sizeof" };
+
+  private static RevStringByLengthComparator byLenghtDescending = new RevStringByLengthComparator();
+  static {
+    // sort keyword arrays by keyword length (descending)
+    Arrays.sort( kwPredefVal, byLenghtDescending);
+    Arrays.sort( kwType, byLenghtDescending);
+    Arrays.sort( kwStmt, byLenghtDescending);
+    Arrays.sort( kwOperator, byLenghtDescending);
+  }
+
+  public C_Categoriser()
+  {
+    Comparator byLength = new RevStringByLengthComparator();
   }
 
   public void setInput( Segment input)
@@ -47,115 +70,173 @@ public class C_Tokeniser extends AbstractCategoriser
       token = new Token();
     }
 
-    token.categoryId = CategoryConstants.NORMAL;
-    char c;
-    matchToken: for (; true; c = input.next(), token.start = input.getIndex()) {
-
-      consumeChars( matchWhitespace());
-      token.start = input.getIndex();
-      c = input.current();
-      int matchLen;
-
-      switch (c) {
-        case CharacterIterator.DONE:
-        // will return a zero length token
-        break matchToken;
-        case '\"':
-          // String
-          readString();
-          token.categoryId = CategoryConstants.STRINGVAL;
-        break matchToken;
-
-        case '\'':
-          // char const. we allow constants of arbitraty length here
-          readCharConst();
-          token.categoryId = CategoryConstants.STRINGVAL;
-        break matchToken;
-
-        case '/':
-          // comments or operator?
-          switch (LA( 1)) {
-            case '*':
-              boolean isMultiline = readMLComment();
-              // TODO makr as multiline
-              token.categoryId = CategoryConstants.COMMENT1;
-            break;
-            case '/':
-              readEOLComment();
-              token.categoryId = CategoryConstants.COMMENT2;
-            break;
-            default:
-              input.next(); // consume '/'
-              token.categoryId = CategoryConstants.OPERATOR;
-            break;
-          }
-        break matchToken;
-        case '#': // preprocessor directive
-          input.next(); // consume '#'
-          matchLen = matchWhitespace();
-          matchLen += matchWord();
-          consumeChars( matchLen);
-          token.categoryId = CategoryConstants.KEYWORD2;
-        break matchToken;
-
-        default:
-          if ((matchLen = matchNumber()) > 0) {
-            token.categoryId = CategoryConstants.NUMERICVAL;
-            consumeChars( matchLen);
-            break matchToken;
-          }
-          else if ((matchLen = matchKW_PredefVal()) > 0) {
-            token.categoryId = CategoryConstants.PREDEFVAL;
-            consumeChars( matchLen);
-            break matchToken;
-          }
-          else if ((matchLen = matchOperator()) > 0) {
-            token.categoryId = CategoryConstants.OPERATOR;
-            consumeChars( matchLen);
-            break matchToken;
-          }
-          else if ((matchLen = matchKW_Type()) > 0) {
-            token.categoryId = CategoryConstants.TYPE;
-            consumeChars( matchLen);
-            break matchToken;
-          }
-          else if ((matchLen = matchKW_stmt()) > 0) {
-            token.categoryId = CategoryConstants.IDENTIFIER1;
-            consumeChars( matchLen);
-            break matchToken;
-          }
-          else if ((matchLen = matchIdentifier1()) > 0) {
-            token.categoryId = CategoryConstants.IDENTIFIER1;
-            consumeChars( matchLen);
-            break matchToken;
-          }
-          else if ((matchLen = matchIdentifier1()) > 0) {
-            token.categoryId = CategoryConstants.IDENTIFIER1;
-            consumeChars( matchLen);
-            break matchToken;
-          }
-          else {
-            // still no category found...
-            int kk = 0;
-            // TODO
-          }
-      } // switch
-    }// matchToken
-
-    token.length = input.getIndex() - token.start;
+    getToken( token);
     return token;
   }
 
   /**
-   * @return
+   * Überschrieben, um
    */
-  private int matchKW_stmt()
+  public void insertUpdate( Element elem)
   {
-    return matchInWordlist( kwStmt);
+    // TODO Auto-generated method stub
+    //throw new java.lang.UnsupportedOperationException("insertUpdate not
+    // implemented");
+
   }
 
   /**
-   * @return the length of the match or <code>0</code> if no match was found.
+   * Überschrieben, um
+   */
+  public void removeUpdate( Element line)
+  {
+    // TODO Auto-generated method stub
+    //throw new java.lang.UnsupportedOperationException("removeUpdate not
+    // implemented");
+
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see swing.text.highlight.categoriser.Categoriser#closeInput()
+   */
+  public void closeInput()
+  {
+    // TODO Auto-generated method stub
+  }
+
+  ///////////////////////////////////////////////////////////
+  // categoriser methods
+  ///////////////////////////////////////////////////////////
+  /**
+   * @param token
+   *          the token to initialise.
+   */
+  private void getToken( Token token)
+  {
+    token.categoryId = CategoryConstants.NORMAL;
+    token.start = input.getIndex();
+    int matchLen;
+    char c = input.current();
+
+    switch (c) {
+      case CharacterIterator.DONE:
+      // will return a zero length token
+      break;
+      case '\"':
+        // String
+        readString();
+        token.categoryId = CategoryConstants.STRINGVAL;
+      break;
+
+      case '\'':
+        // char const. we allow constants of arbitraty length here
+        readCharConst();
+        token.categoryId = CategoryConstants.STRINGVAL;
+      break;
+
+      case '/':
+        // comments or operator?
+        switch (LA( 1)) {
+          case '*':
+            boolean isMultiline = readMLComment();
+            // TODO makr as multiline
+            token.categoryId = CategoryConstants.COMMENT1;
+          break;
+          case '/':
+            readEOLComment();
+            token.categoryId = CategoryConstants.COMMENT2;
+          break;
+          default:
+            input.next(); // consume '/'
+            token.categoryId = CategoryConstants.OPERATOR;
+          break;
+        }
+      break;
+      case '#': // preprocessor directive
+        input.next(); // consume '#'
+        matchLen = matchWhitespace();
+        matchLen += matchWord();
+        consumeChars( matchLen);
+        token.categoryId = CategoryConstants.KEYWORD2;
+      break;
+
+      default:
+        if ((matchLen = matchWhitespace()) > 0) {
+          token.categoryId = CategoryConstants.NORMAL;
+          consumeChars( matchLen);
+        }
+        else if ((matchLen = matchNumber()) > 0) {
+          token.categoryId = CategoryConstants.NUMERICVAL;
+          consumeChars( matchLen);
+        }
+        else if ((matchLen = matchOperator()) > 0) {
+          token.categoryId = CategoryConstants.OPERATOR;
+          consumeChars( matchLen);
+        }
+        else {
+          // now try to match a keyword
+          matchLen = matchWord();
+          if (matchLen == 0) {
+            /*
+             * if we dont get a keyword here, it is any character not handled
+             * somewhere above
+             */
+            // treat as normal text
+            token.categoryId = CategoryConstants.NORMAL;
+            input.next(); // consume char
+            break;
+          }
+
+          assert matchLen > 0 : "unrecognized char in scanner: " + LA( 0);
+          if (isKW_PredefVal( matchLen)) {
+            token.categoryId = CategoryConstants.PREDEFVAL;
+            consumeChars( matchLen);
+          }
+          else if (isKW_Type( matchLen)) {
+            token.categoryId = CategoryConstants.TYPE;
+            consumeChars( matchLen);
+          }
+          else if (isKW_stmt( matchLen)) {
+            token.categoryId = CategoryConstants.KEYWORD1;
+            consumeChars( matchLen);
+          }
+          else if (isIdentifier1( matchLen)) {
+            token.categoryId = CategoryConstants.IDENTIFIER1;
+            consumeChars( matchLen);
+          }
+          else if (isIdentifier2( matchLen)) {
+            token.categoryId = CategoryConstants.IDENTIFIER2;
+            consumeChars( matchLen);
+          }
+          else {
+            // still no category found...
+            // treat matched word as normal text
+            token.categoryId = CategoryConstants.NORMAL;
+            consumeChars( matchLen);
+          }
+        }
+      break; // default
+    } // switch
+
+    token.length = input.getIndex() - token.start;
+  }
+
+  /**
+   * @param lenght
+   *          the length of the region that must match.
+   * @return <code>true</code> if a match was found, otherwise
+   *         <code>false</code>.
+   */
+  private boolean isKW_stmt( int length)
+  {
+    return matchInWordlist( length, kwStmt);
+  }
+
+  /**
+   * @return the length of the matching text or <code>0</code> if no match was
+   *         found.
    */
   private int matchNumber()
   {
@@ -164,91 +245,121 @@ public class C_Tokeniser extends AbstractCategoriser
   }
 
   /**
-   * @return the length of the match or <code>0</code> if no match was found.
+   * @return the length of the matching text or <code>0</code> if no match was
+   *         found.
    */
   private int matchOperator()
   {
-    // TODO greedy loop implementieren für '+=sizeof--' o.ä.
-    int len = matchInWordlist( kwOperator);
-    // non keyword operators
-    // match anything that looks like a sequence of operators...
-    for (char c = LA( len); c != CharacterIterator.DONE; c = LA( ++len)) {
-      if (Character.isJavaIdentifierPart( c) || Character.isWhitespace( c)
-          || ";()[]{}'".indexOf( c) >= 0) {
-        break;
+    int len = 0;
+    greedyLoop: while (true) {
+      int kwLen = matchWord();
+      if (kwLen > 0) {
+        if (matchInWordlist( kwLen, kwOperator)) {
+          // got a keyword operator
+          len += kwLen;
+        }
+        else {
+          // got a word, but not an operator keyword
+          break greedyLoop;
+        }
+      }
+      else {
+        // didnt get a word--> non keyword operators
+        // greedyly match anything that looks like a sequence of operators...
+        for (char c = LA( len); c != CharacterIterator.DONE; c = LA( ++len)) {
+          if (";(){}'".indexOf( c) >= 0) {
+            break greedyLoop;
+          }
+        }
       }
     }
     return len;
   }
 
   /**
-   * @return the length of the match or <code>0</code> if no match was found.
+   * @param lenght
+   *          the length of the region that must match.
+   * @return <code>true</code> if a match was found, otherwise
+   *         <code>false</code>.
    */
-  private int matchKW_Type()
+  private boolean isKW_Type( int length)
   {
-    return matchInWordlist( kwType);
+    return matchInWordlist( length, kwType);
   }
 
   /**
-   * @return the length of the match or <code>0</code> if no match was found.
+   * @param lenght
+   *          the length of the region that must match.
+   * @return <code>true</code> if a match was found, otherwise
+   *         <code>false</code>.
    */
-  protected int matchKW_PredefVal()
+  protected boolean isKW_PredefVal( int length)
   {
     /* C++ kennt true, false */
-    return matchInWordlist( kwPredefVal);
+    return matchInWordlist( length, kwPredefVal);
   }
 
   /**
-   * Looks if a region starting at the current scanner input position matches a
+   * Looks if a region starting at the current scanner input position equals a
    * String in <code>wordlist</code>.
    * 
+   * @param lenght
+   *          the length of the region that must match.
    * @param wordlist
-   *          the string that may match.
-   * @return the length of the match or <code>0</code> if no match was found.
+   *          the strings that may match.
+   * @return <code>true</code> if a match was found, otherwise
+   *         <code>false</code>.
    */
-  private int matchInWordlist( final String[] wordlist)
+  private boolean matchInWordlist( int length, final String[] wordlist)
   {
     for (int i = 0; i < wordlist.length; i++ ) {
       int len;
-      if ((len = AbstractCategoriser.regionMatches( false, input, input
-          .getIndex(), wordlist[i])) > 0)
-        return len;
+      if (wordlist[i].length() == length
+          && AbstractCategoriser.regionMatches( false, input, input.getIndex(),
+              wordlist[i]) > 0)
+        return true;
     }
-    return 0; // no match
+    return false; // no match
 
   }
 
   /**
-   * @return the length of the match or <code>0</code> if no match was found.
+   * @param identifierLen
+   *          the length of the region that must match.
+   * @return the length of the matching text or <code>0</code> if no match was
+   *         found.
    */
-  private int matchIdentifier1()
+  private boolean isIdentifier1( int identifierLen)
   {
     // TODO Auto-generated method stub
     /*
      * mittels vom Anwender gefüllter Worttabelle (auc Document-Attribute?)
      * matchen
      */
-    return 0; // no match
+    return false; // no match
   }
 
   /**
-   * 
+   * @param identifierLen
+   *          the length of the region that must match.
+   * @return the length of the matching text or <code>0</code> if no match was
+   *         found.
    */
-  private void readWord()
+  private boolean isIdentifier2( int identifierLen)
   {
-    char c = input.current();
-    if (Character.isJavaIdentifierStart( c)) {
-      c = input.next();
-      while (Character.isJavaIdentifierPart( c)) {
-        c = input.next();
-      }
-    }
+    // TODO Auto-generated method stub
+    /*
+     * mittels vom Anwender gefüllter Worttabelle (auc Document-Attribute?)
+     * matchen
+     */
+    return false; // no match
   }
 
   /**
    * Matches an Identifer or a keyword.
    * 
-   * @return the length of the match or <code>0</code> if no match was found.
+   * @return the length of the matching text or <code>0</code> if no match was
+   *         found.
    */
   private int matchWord()
   {
@@ -266,7 +377,8 @@ public class C_Tokeniser extends AbstractCategoriser
   /**
    * *
    * 
-   * @return the length of the match or <code>0</code> if no match was found.
+   * @return the length of the matching text or <code>0</code> if no match was
+   *         found.
    */
   private int matchWhitespace()
   {
@@ -283,10 +395,18 @@ public class C_Tokeniser extends AbstractCategoriser
    */
   private void readCharConst()
   {
-    // TODO Auto-generated method stub
-    throw new java.lang.UnsupportedOperationException(
-        "readCharCOnst not implemented");
-
+    char c = input.next();
+    for (; c != CharacterIterator.DONE; c = input.next()) {
+      switch (c) {
+        case '\\':
+          input.next();
+        break;
+        case '\'':
+        case '\n':
+          input.next();
+          return;
+      }
+    }
   }
 
   /**
@@ -349,38 +469,6 @@ public class C_Tokeniser extends AbstractCategoriser
           return;
       }
     }
-  }
-
-  /**
-   * Überschrieben, um
-   */
-  public void insertUpdate( Element elem)
-  {
-    // TODO Auto-generated method stub
-    //throw new java.lang.UnsupportedOperationException("insertUpdate not
-    // implemented");
-
-  }
-
-  /**
-   * Überschrieben, um
-   */
-  public void removeUpdate( Element line)
-  {
-    // TODO Auto-generated method stub
-    //throw new java.lang.UnsupportedOperationException("removeUpdate not
-    // implemented");
-
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see swing.text.highlight.categoriser.Categoriser#closeInput()
-   */
-  public void closeInput()
-  {
-    // TODO Auto-generated method stub
   }
 
   ///////////////////////////////////////////////////////////
