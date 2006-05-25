24b559c185e5ed83835ef74326765812b3e65545
fifteenknots505 <fifteenknots505@gmail.com>
2006-05-25 17:00:18 +0000

    added/reworked documentation

--- src/de/marw/nacre/highlight/HighlightingKit.java
+++ src/de/marw/nacre/highlight/HighlightingKit.java
@@ -1,6 +1,6 @@
 /* $Id$ */
 
-// Copyright © 2004 Martin Weber
+// Copyright © 2004-2006 Martin Weber
 
 package de.marw.javax.swing.text.highlight;
 
@@ -19,33 +19,53 @@ import de.marw.javax.swing.text.highlight.categoriser.Categoriser;
 /**
  * This kit supports a fairly minimal handling of editing a programming language
  * text content. It supports syntax highlighting and produces the lexical
- * structure of the document as best it can.
- * </p>
- * Using a HighlightingKit to obtain syntax highlighting in a JEditorPane ist
- * straitforward: Simply instantiate a subclass of <code>HighlightingKit</code>,
- * then
+ * structure of the document as best it can. <br>
+ * A <code>HighlightingKit</code> or one of its subclasses can be used with
+ * any JTextcomponent.
+ * <p>
+ * Using a HighlightingKit to have syntax highlighting in a JEditorPane is
+ * straitforward:
+ * <ul>
+ * <li>First instantiate a subclass of <code>HighlightingKit</code> according
+ * to the programming language you want to highlight, </li>
+ * <li>then
  * {@link javax.swing.JEditorPane#setEditorKitForContentType(java.lang.String, javax.swing.text.EditorKit)
- * set the editor kit(s) to use} for the given content type. It is possible to
- * set multiple editor kits, each for a different content type. After that,
- * {@link javax.swing.JEditorPane#setContentType(java.lang.String) set the content type}
- * to install the appropriate subclass of <code>HighlightingKit</code>, thus
- * activating the kind of highlighting You want.
- * 
- * <pre>
+ * register the editor kit(s) to use} for the content type of the programming
+ * language.</li>
+ * <li>After that,
+ * {@link javax.swing.JEditorPane#setContentType(java.lang.String) set the selected content type}
+ * to activate highlighting.</li>
+ * </ul>
+ * </p>
+ * <code><pre>
+ * JEditorPane editor = new JEditorPane();
+ * // ...
+ * // add highlighting for Java code
+ * HighlightingKit kit = new JavaHighlightingKit();
+ * String contentTyoe = kit.getContentType();
+ * editor.setEditorKitForContentType( contentType, kit);
+ * //...
+ * // activate highlighting 
+ * editor.setContentType( contentType);
+ * </pre></code>
+ * <p>
+ * It is also possible to register multiple editor kits, each for a different
+ * programming language and later to select the style of highlighting on demand.
+ * </p>
+ * <code><pre>
  * JEditorPane editor = new JEditorPane();
  * // ...
  * HighlightingKit kit1 = new JavaHighlightingKit();
  * HighlightingKit kit2 = new CHighlightingKit();
  * 
- * editor.setEditorKitForContentType( kit1.getContentType(), kit);
- * editor.setEditorKitForContentType( kit2.getContentType(), kit);
- * // add more EditorKits to support different content types here...
+ * editor.setEditorKitForContentType( kit1.getContentType(), kit1);
+ * editor.setEditorKitForContentType( kit2.getContentType(), kit2);
+ * // more EditorKits to support different content types here...
  * //...
  * 
- * //  activate the kind of highlighting for Java code
+ * // activate highlighting for Java code
  * editor.setContentType( &quot;text/x-java&quot;);
- * </pre>
- * 
+ * </pre></code>
  * </p>
  * 
  * @author Martin Weber
@@ -86,22 +106,26 @@ public abstract class HighlightingKit extends DefaultEditorKit implements
    * <code>HighlightingKit</code> installed.
    * <p>
    * NOTE for subclass implementors: The color and font style informations
-   * returned here are designed to be shared with any <code>View</code> that
-   * gets created by this view factory. Subclasses are expected to return a
-   * <strong>static variable</strong> (class variable) here.
+   * returned here are designed to be shared with any
+   * <code>[@linkplain View}</code> that gets created by this view factory.
+   * Subclasses are expected to return a <strong>static variable</strong>
+   * (class variable) here.
    * </p>
    */
   public abstract CategoryStyles getCategoryStyles();
 
   /**
-   * Returns a Map that specifies each category as a <strong>localized </strong>
-   * string that can be used as a label. If the returned Map does not contain a
-   * specific Category, this means the editor kit implementation does not
-   * highlight any text of this missing category.
+   * Returns a Map that specifies each category as a <strong>localized</strong>
+   * string that can be used as a label.
+   * <p>
+   * If the returned Map does not contain a specific <code>Category</code>,
+   * this means the editor kit implementation does not highlight any text of
+   * that category.
    * 
    * @param locale
    *        the locale for which the localised strings are desired or
    *        <code>null</code> if the default locale should be used.
+   * @see Category
    */
   public abstract Map<Category, String> getCategoryDescriptions( Locale locale);
 
@@ -115,7 +139,7 @@ public abstract class HighlightingKit extends DefaultEditorKit implements
    * Fetches the Categoriser used for highlighting text of this document or
    * <code>null</code> if no highlighting is to be done.
    */
-  protected final Categoriser getCategoriser()
+  private Categoriser getCategoriser()
   {
     if (categoriser == null) {
       categoriser = createCategoriser();
@@ -126,8 +150,7 @@ public abstract class HighlightingKit extends DefaultEditorKit implements
 
   /**
    * Fetches a factory that is suitable for producing views of any models that
-   * are produced by this kit. The default is to have the UI produce the
-   * factory, so this method has no implementation.
+   * are produced by this kit.
    * 
    * @return the view factory
    */
