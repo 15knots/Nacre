99f54d613f6e54dc4bb522dc88252034b07a6b7e
fifteenknots505 <fifteenknots505@gmail.com>
2006-11-10 21:47:07 +0000

    Factored out class DemoFrame

---
+++ demo/DemoFrame.java
@@ -0,0 +1,87 @@
+// $Header$
+/*
+ * Copyright 2006 by Martin Weber
+ */
+
+import java.awt.BorderLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.text.Document;
+
+
+/**
+ * The JFrame used for all demo applications.
+ * 
+ * @author weber
+ */
+public abstract class DemoFrame extends JFrame
+{
+
+  // TODO theoretically, highlighting will work with any JTextComponent, check
+  // this type
+  private JEditorPane editor;
+
+  public DemoFrame()
+  {
+    super();
+    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+    getContentPane().setLayout( new BorderLayout());
+    JScrollPane scroller= new JScrollPane();
+    scroller.setViewportView( getEditor());
+
+    getContentPane().add( "Center", scroller);
+  }
+
+  /**
+   * Gets the first part of the text for the frame title.
+   */
+  protected abstract String getDemoName();
+
+  /**
+   * Gets the editor component that showe the highlighted text.
+   */
+  protected JEditorPane getEditor()
+  {
+    if (editor == null) {
+      editor= new JEditorPane();
+      // change the frames title
+      PropertyChangeListener updater= new TitleUpdater();
+      editor.addPropertyChangeListener( "document", updater);
+      editor.addPropertyChangeListener( "editorKit", updater);
+    }
+    return editor;
+  }
+
+  // //////////////////////////////////////////////////////////////////
+  private class TitleUpdater implements PropertyChangeListener
+  {
+    private String docName;
+
+    private String contentType;
+
+    public void propertyChange( PropertyChangeEvent evt)
+    {
+      String propertyName= evt.getPropertyName();
+      if (propertyName == "document") {
+        Document doc= (Document) evt.getNewValue();
+        Object docDesc= doc.getProperty( Document.StreamDescriptionProperty);
+        if (docDesc != null) {
+          docName= docDesc.toString();
+        }
+      }
+      else if (propertyName == "editorKit") {
+        String conType= editor.getContentType();
+        if (conType != null) {
+          contentType= conType;
+        }
+      }
+      setTitle( getDemoName() + ": " + (docName != null
+        ? docName : "") + (contentType != null
+        ? (" (" + contentType + ")") : ""));
+    }
+  }
+}
--- demo/HighlightKitDemo.java
+++ demo/HighlightKitDemo.java
@@ -1,17 +1,13 @@
 /* $Id$ */
 
 // Copyright 2004-2006 Martin Weber
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Point;
+import java.awt.HeadlessException;
 import java.io.File;
 import java.io.FileReader;
 
 import javax.swing.JEditorPane;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.text.Document;
 
 import de.marw.nacre.editorkits.C_HighlightingKit;
 import de.marw.nacre.editorkits.JavaHighlightingKit;
@@ -21,82 +17,43 @@ import de.marw.nacre.highlight.categoriser.Category;
 
 
 /**
- * Simple wrapper around JEditorPane to browse program code using the
- * HighlightingKit plug-in. Run with
+ * This demo shows how to set up a <code>JEditorPane</code> to support both
+ * highlighting of <em>Java</em> and <em>C</em> program code. The actual
+ * style for highlighting text is chosen depending of the input file's filename
+ * extension. <br>
+ * Run with
  * 
  * <pre>
- *   Usage:
- *        java HighlightKitTest filename
+ *                Usage:
+ *                     java HighlightKitTest filename
  * </pre>
  */
-public class HighlightKitDemo
+public class HighlightKitDemo extends DemoFrame
 {
 
   /**
-   * This is used to test for appropriate handling of tokens that span multiple
-   * lines. <code>true</code>, if the first line of text to categorise is not
-   * at offset zero.
    */
-  private static final boolean TEST_MULTILINE_TOKEN_PROOFNESS= false;
-
-  public static void main( String[] args)
+  public HighlightKitDemo() throws HeadlessException
   {
-    if (args.length != 1) {
-      System.err.println( "need filename argument");
-      System.exit( 1);
-    }
-    // create an editor pane
-    JEditorPane editor= new JEditorPane();
     // install an editor kit that does syntax highlighting
-    configureEditor( editor);
+    configureEditor( getEditor());
+  }
 
-    try {
-      // read file
-      File file= new File( args[0]);
-      // select highlighting
-      if (args[0].endsWith( ".c")) {
-        editor.setContentType( "text/x-c-src");
-      }
-      else if (args[0].endsWith( ".java")) {
-        editor.setContentType( "text/x-java");
-      }
-      editor.read( new FileReader( file), file);
-      // GUI setup
-      JScrollPane scroller= new JScrollPane();
-      scroller.setViewportView( editor);
-
-      Document doc= editor.getDocument();
-      Object docDesc= doc.getProperty( Document.StreamDescriptionProperty);
-      JFrame f=
-          new JFrame( "HighlightingKit: " + docDesc.toString() + " ("
-              + editor.getContentType() + ")");
-      f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
-      f.getContentPane().setLayout( new BorderLayout());
-      f.getContentPane().add( "Center", scroller);
-
-      f.pack();
-      f.setSize( 211, 701);
-      if (TEST_MULTILINE_TOKEN_PROOFNESS) {
-        // caret in letzte Zeile (test, ob repaint() immer in erster Zeile
-        // anf�ngt)
-        f.setSize( 211, 301);
-        editor.setCaretPosition( editor.getDocument().getLength() - 1);
-        scroller.getViewport().setViewPosition( new Point( 0, 9999));
-      }
-
-      f.setVisible( true);
-    }
-    catch (Throwable e) {
-      e.printStackTrace();
-    }
+  @Override
+  protected String getDemoName()
+  {
+    return "Java or C Demo";
   }
 
   /**
+   * Installs editor kits for C and Java for syntax highlighting. <br>
+   * Sets a font suitable for syntax highlighting. <br>
+   * Customises the style for rendering the <code>Category.COMMENT_2</code>.
+   * 
    * @param editor
    */
-  protected static void configureEditor( JEditorPane editor)
+  protected void configureEditor( JEditorPane editor)
   {
-    // editor.setBackground( Color.white);
     // add C highlighting
     HighlightingKit kit= new C_HighlightingKit();
     editor.setEditorKitForContentType( kit.getContentType(), kit);
@@ -107,6 +64,7 @@ public class HighlightKitDemo
 
     // highlighting requires a font that has the same width, regardless
     // whether the font is rendered PLAIN, BOLD or ITALIC.
+    
     // editor.setFont( new Font( "Courier", Font.PLAIN, 12));
     // editor.setFont( new Font( "Courier 10 Pitch", Font.PLAIN, 12));
     editor.setFont( new Font( "Courier New", Font.PLAIN, 13));
@@ -120,7 +78,7 @@ public class HighlightKitDemo
     // editor.setFont( new Font( "SansSerif", Font.PLAIN, 13));
 
     // select C highlighting
-    editor.setContentType( "text/x-c-src");
+    // editor.setContentType( "text/x-c-src");
     // editor.setContentType( "text/x-java");
 
     // customise colour and font style of hightlighting
@@ -129,6 +87,40 @@ public class HighlightKitDemo
             .getContentType());
     CategoryStyles styles= kit.getCategoryStyles();
     styles.setColor( Category.COMMENT_2, Color.YELLOW);
+  }
+
+  /**
+   */
+  public static void main( String[] args)
+  {
+    if (args.length != 1) {
+      System.err.println( "need filename argument");
+      System.exit( 1);
+    }
+
+    // GUI setup
+    HighlightKitDemo frame= new HighlightKitDemo();
+    JEditorPane editor= frame.getEditor();
+    try {
+      // read file
+      File file= new File( args[0]);
+      editor.read( new FileReader( file), file);
+      // select highlighting depending on filename extension
+      if (args[0].endsWith( ".c")) {
+        editor.setContentType( "text/x-c-src");
+      }
+      else if (args[0].endsWith( ".java")) {
+        editor.setContentType( "text/x-java");
+      }
+
+      frame.pack();
+      frame.setSize( 211, 701);
+
+      frame.setVisible( true);
+    }
+    catch (Throwable e) {
+      e.printStackTrace();
+    }
   }
 
 }
